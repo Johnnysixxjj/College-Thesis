5cd8f35bee28124bae1aa5844d21abe36c815905
Johnnysixxjj <111279511+Johnnysixxjj@users.noreply.github.com>
2022-09-14 09:20:44 +0800

    Delete pet_groomers directory

--- pet_groomers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Services, PetInfo, Appointment
-# Register your models here.
-
-admin.site.register(Services)
-admin.site.register(PetInfo)
-admin.site.register(Appointment)
