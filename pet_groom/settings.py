3c00a3374b623345c4b6d596f2fa8698e2d2dd1d
Johnnysixxjj <111279511+Johnnysixxjj@users.noreply.github.com>
2022-09-14 09:34:31 +0800

    Update settings.py

--- pet_groom/settings.py
+++ pet_groom/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('secret_key', 'some_random_default_string')
+SECRET_KEY = 'django-insecure-u&%dng5w6@-dkuj%b17m8v*(6l_7t+8p$#5!_tp&!xqg9_m!e!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(DEBUG, False)
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'tinymce',
     'based',
 ]
 
